aaaa75d22c3ba83261cd34fa6947347dbe10685a
perhamm <34609259+perhamm@users.noreply.github.com>
2018-05-21 11:48:29 +0300

    Create zbxemail_settings.py

---
+++ zbxemail_settings.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+#Zabbix server settings
+zbx_server = 'http://vds01-pizab--01.ctd.tn.corp/zabbix'  # zabbix server full url
+zbx_api_user = 'Admin'
+zbx_api_pass = 'YDOe}TOehW?S'
+zbx_graph_width = '600'
+zbx_graph_height = '200'
+zbx_graph_period = '1800'
+zbx_tmp_path = '/var/tmp/zbxemail'
+zbx_alert_path = '/usr/lib/zabbix/alertscripts'
+#Email settings
+email_from = 'zabbix@ctd.transneft.ru'
+email_username = ''
+email_password = ''
+email_smtp = '10.209.101.253'
+
+#Template settings
+company = 'АО "Транснефть-Диаскан"'
+
