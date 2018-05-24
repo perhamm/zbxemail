e6d54558781f83b26c90ac8bdc67fb1721284075
perhamm <34609259+perhamm@users.noreply.github.com>
2018-05-24 16:59:44 +0300

    Update zbxemail_settings.py

--- zbxemail_settings.py
+++ zbxemail_settings.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 #Zabbix server settings
-zbx_server = 'http://vds01-pizab--01.local/zabbix'  # zabbix server full url
+zbx_server = 'http://zab.local/zabbix'  # zabbix server full url
 zbx_api_user = 'readonly'
 zbx_api_pass = '123qweQWE'
 zbx_graph_width = '600'
@@ -13,7 +13,7 @@ zbx_alert_path = '/usr/lib/zabbix/alertscripts'
 email_from = 'zabbix@local'
 email_username = ''
 email_password = ''
-email_smtp = '10.209.1.253'
+email_smtp = '1.1.1.1'
 
 #Template settings
 company = 'АО "Рога и Копыта"'
